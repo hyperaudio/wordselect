3109fd3e9c4f2e16135f4edbb43b7c0c96eb72e1
cubiq <matteo@cubiq.org>
2013-09-08 19:44:00 +0200

    core functionality

---
+++ wordselect.js
@@ -0,0 +1,249 @@
+var WordSelect = (function (window, document) {
+
+	function addTagHelpers (el) {
+		var text = el.innerText.split(' ');
+
+		el.innerHTML = '<a>' + text.join(' </a><a>') + '</a>';
+	}
+
+	function hasClass (e, c) {
+		if ( !e ) return false;
+
+		var re = new RegExp("(^|\\s)" + c + "(\\s|$)");
+		return re.test(e.className);
+	};
+
+	function addClass (e, c) {
+		if ( hasClass(e, c) ) {
+			return;
+		}
+
+		var newclass = e.className.split(' ');
+		newclass.push(c);
+		e.className = newclass.join(' ');
+	};
+
+	 function removeClass (e, c) {
+		if ( !hasClass(e, c) ) {
+			return;
+		}
+
+		var re = new RegExp("(^|\\s)" + c + "(\\s|$)", 'g');
+		e.className = e.className.replace(re, ' ');
+	};
+
+	function WordSelect (el, options) {
+		this.element = document.querySelector(el);
+
+		this.options = {
+			addHelpers: false,
+			touch: true,
+			mouse: true,
+			threshold: 10
+		};
+
+		for ( var i in options ) {
+			this.options[i] = options[i];
+		}
+
+		if ( this.options.addHelpers ) {
+			addTagHelpers(this.element);
+		}
+
+		this.words = this.element.querySelectorAll('a');
+		this.wordsCount = this.words.length;
+
+		if ( this.options.touch ) {
+			this.element.addEventListener('touchstart', this, false);
+		}
+
+		if ( this.options.mouse ) {
+			this.element.addEventListener('mousedown', this, false);
+		}
+	}
+
+	WordSelect.prototype.handleEvent = function (e) {
+		switch (e.type) {
+			case 'mousedown':
+				if ( e.which !== 1 ) {
+					break;
+				}
+			case 'touchstart':
+				this.start(e);
+				break;
+			case 'touchmove':
+			case 'mousemove':
+				this.move(e);
+				break;
+			case 'touchend':
+			case 'mouseup':
+				this.end(e);
+				break;
+		}
+	};
+
+	WordSelect.prototype.start = function (e) {
+		e.preventDefault();
+
+		var point = e.touches ? e.touches[0] : e;
+
+		this.selectStarted = false;
+		this.startX = e.pageX;
+		this.startY = e.pageY;
+
+		if ( this.options.mouse ) {
+			this.element.addEventListener('mousemove', this, false);
+			this.element.addEventListener('mouseup', this, false);
+		}
+
+		if ( this.options.touch ) {
+			this.element.addEventListener('touchmove', this, false);
+			this.element.addEventListener('touchend', this, false);
+		}
+
+		if ( hasClass(e.target, 'selected') ) {
+			this.dragTimeout = setTimeout(this.dragStart.bind(this, e), 500);
+		}
+	};
+
+	WordSelect.prototype.selectStart = function (e) {
+		var target = e.target,
+			tmp;
+
+		if ( target == this.element ) {
+			return;
+		}
+
+		this.selectStarted = true;
+
+		this.currentWord = target;
+
+		removeClass(this.element.querySelector('.first'), 'first');
+		removeClass(this.element.querySelector('.last'), 'last');
+
+		if ( this.words[this.startPosition] === target ) {
+			tmp = this.startPosition;
+			this.startPosition = this.endPosition;
+			this.endPosition = tmp;
+			return;
+		}
+
+		if ( this.words[this.endPosition] === target ) {
+			return;
+		}
+
+		for ( var i = 0; i < this.wordsCount; i++ ) {
+			if ( this.words[i] == target ) {
+				this.startPosition = i;
+			}
+
+			removeClass(this.words[i], 'selected');
+		}
+
+		this.endPosition = this.startPosition;
+
+		addClass(target, 'selected');
+	};
+
+	WordSelect.prototype.move = function (e) {
+		var point = e.changedTouches ? e.changedTouches[0] : e,
+			target = e.touches ? document.elementFromPoint(point.pageX, point.pageY) : point.target,
+			endPosition;
+
+		if ( Math.abs(point.pageX - this.startX) < this.options.threshold &&
+			Math.abs(point.pageY - this.startY) < this.options.threshold ) {
+			return;
+		}
+
+		clearTimeout(this.dragTimeout);
+
+		if ( !this.selectStarted ) {
+			this.selectStart(e);
+			return;
+		}
+
+		if ( target == this.element || target == this.currentWord ) {
+			return;
+		}
+
+		for ( var i = 0; i < this.wordsCount; i++ ) {
+			if ( this.words[i] == target ) {
+				endPosition = i;
+			}
+
+			if ( ( endPosition === undefined && i >= this.startPosition ) ||
+				( endPosition !== undefined && i <= this.startPosition ) ||
+				endPosition == i ) {
+				addClass(this.words[i], 'selected');
+			} else {
+				removeClass(this.words[i], 'selected');
+			}
+		}
+
+		this.currentWord = target;
+		this.endPosition = endPosition;
+	};
+
+	WordSelect.prototype.end = function (e) {
+		clearTimeout(this.dragTimeout);
+
+		var start = Math.min(this.startPosition, this.endPosition),
+			end = Math.max(this.startPosition, this.endPosition);
+
+		addClass(this.words[start], 'first');
+		addClass(this.words[end], 'last');
+
+		if ( this.options.touch ) {
+			this.element.removeEventListener('touchmove', this, false);
+			this.element.removeEventListener('touchend', this, false);
+		}
+
+		if ( this.options.mouse ) {
+			this.element.removeEventListener('mousemove', this, false);
+			this.element.removeEventListener('mouseup', this, false);
+		}
+	};
+
+	WordSelect.prototype.clearSelection = function () {
+		removeClass(this.element.querySelector('.first'), 'first');
+		removeClass(this.element.querySelector('.last'), 'last');
+
+		var selected = this.element.querySelectorAll('.selected');
+		for ( var i = 0, l = selected.length; i < l; i++ ) {
+			removeClass(selected[i], 'selected');
+		}
+	};
+
+	WordSelect.prototype.dragStart = function (e) {
+		e.stopPropagation();
+
+		if ( this.options.touch ) {
+			this.element.removeEventListener('touchmove', this, false);
+			this.element.removeEventListener('touchend', this, false);
+		}
+
+		if ( this.options.mouse ) {
+			this.element.removeEventListener('mousemove', this, false);
+			this.element.removeEventListener('mouseup', this, false);
+		}
+
+		var point = e.changedTouches ? e.changedTouches[0] : e;
+
+		if ( this.options.onDragStart ) {
+			this.options.onDragStart.call(this);
+		}
+	};
+
+	WordSelect.prototype.destroy = function () {
+		this.element.removeEventListener('touchstart', this, false);
+		this.element.removeEventListener('touchmove', this, false);
+		this.element.removeEventListener('touchend', this, false);
+
+		this.element.removeEventListener('mousedown', this, false);
+		this.element.removeEventListener('mousemove', this, false);
+		this.element.removeEventListener('mouseup', this, false);
+	}
+
+	return WordSelect;
+
+})(window, document);
